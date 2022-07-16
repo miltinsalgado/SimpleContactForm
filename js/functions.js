6badc60bcdc4e5bb5f6c5622ee3853790d6f2a4b
miltinsalgado <miltonsalgadoleandro@gmail.com>
2022-07-15 22:15:44 -0300

    Update Commit

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,8 @@
 function showFormData () {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var tel = document.getElementById("tel").value;
-    var message = document.getElementById("message").value;
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const tel = document.getElementById("tel").value;
+    const message = document.getElementById("message").value;
 
     alert("\nDados recebidos: \n\nNome: " + name + "\nE-mail: " + email + "\nTelefone: " + tel + "\nMensagem: " + message);
 }
